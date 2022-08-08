b1c15bbb25a3d264f2a636bb240cea32d70315a5
Brian Miller <brian@phospher.com>
2022-08-08 13:20:01 -0700

    gha

--- phvalheim-client-installer/selfExtractingExe7Za/buildSelfExtractingExe.bat
+++ phvalheim-client-installer/selfExtractingExe7Za/buildSelfExtractingExe.bat
@@ -3,6 +3,12 @@
 set buildType=%1
 set seven=..\selfExtractingExe7Za\7za\7z.exe
 
-
 "%seven%" a -t7z phvalheim-client-installer.7z .\*
 copy /b ..\selfExtractingExe7Za\7za\7zS.sfx + config.txt + .\phvalheim-client-installer.7z .\phvalheim-client-installer.exe
+
+
+dir .\phvalheim-client-installer.exe
+
+if %buildType%==Release (
+	copy /b .\phvalheim-client-installer.exe ..\..\published_build\phvalheim-client-installer.exe
+)
