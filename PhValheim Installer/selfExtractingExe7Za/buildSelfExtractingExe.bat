8c16f016ee179db7a55dabb4fd3d3501b7820b0f
Brian Miller <brian@phospher.com>
2022-08-08 10:30:02 -0700

    hmm

--- PhValheim Installer/selfExtractingExe7Za/buildSelfExtractingExe.bat
+++ PhValheim Installer/selfExtractingExe7Za/buildSelfExtractingExe.bat
@@ -6,8 +6,8 @@ set seven=..\selfExtractingExe7Za\7za\7z.exe
 
 
 
-del .\phvalheim_installer.7z
-del .\phvalheim_installer.exe
+REM del .\phvalheim_installer.7z
+REM del .\phvalheim_installer.exe
 
 "%seven%" a -t7z phvalheim_installer.7z .\*
 copy /b ..\selfExtractingExe7Za\7za\7zS.sfx + config.txt + .\phvalheim_installer.7z .\phvalheim_installer.exe
