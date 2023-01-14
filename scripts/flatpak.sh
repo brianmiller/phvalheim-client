a717f7066afe28cbd8ba6f37c0b65317d6f68611
Mason Rhodes <mason@lifemd.com>
2023-01-14 14:26:22 -0800

    feat: add Flatpak builder
    probably broken. I do not know what I'm doing

---
+++ scripts/flatpak.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# set the working directory to the script directory
+cd $SCRIPT_DIR/../
+
+# Check if flatpak is installed
+if ! command -v flatpak &> /dev/null
+then
+    echo "flatpak could not be found"
+    exit
+fi
+
+# Check if flatpak-builder is installed
+if ! command -v flatpak-builder &> /dev/null
+then
+    echo "flatpak-builder could not be found"
+    exit
+fi
+
+# Check if flatpak manifest exists
+if [ ! -f "flatpak.json" ]; then
+    echo "flatpak.json could not be found"
+    exit
+fi
+
+# Check if flatpak manifest is valid
+if ! flatpak-builder --show-manifest flatpak.json &> /dev/null
+then
+    echo "flatpak.json is not valid"
+    exit
+fi
+
+# Get latest tag from github
+LATEST_TAG=$(curl -s https://api.github.com/repos/brianmiller/phvalheim-client/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+
+
+# Build flatpak
+flatpak-builder --force-clean --repo=.flatpak-repo ./.flatpak-build-dir ./flatpak.json
+
+# Create single file bundle 
+flatpak build-bundle ./.flatpak-repo ./published_build/phvalheim-client_$LATEST_TAG.flatpak com.github.brianmiller.phvalheim-client
