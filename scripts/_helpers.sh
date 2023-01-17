f72e35624e5c9477388682f8090c9c7740195547
Mason Rhodes <mason@lifemd.com>
2023-01-17 10:22:42 -0800

    fix: ignore failing dude to leading change type

--- scripts/_helpers.sh
+++ scripts/_helpers.sh
@@ -12,7 +12,7 @@ get_uncommited_changes() {
     CHANGES=$(git status --porcelain)
     
     # Remove all ignored files
-    CHANGES=$(echo "$CHANGES" | grep -v -e "^.gitignore" -e "^.gitattributes" -e "^published_build/" -e "^phvalheim-client.csproj" -e "^phvalheim-client.sln")
+    CHANGES=$(echo "$CHANGES" | grep -v -e ".gitignore" -e ".gitattributes" -e "published_build/*" -e "phvalheim-client.csproj" -e "phvalheim-client.sln")
 
     # If there are no changes, then return an empty string
     if [ -z "$CHANGES" ]; then
