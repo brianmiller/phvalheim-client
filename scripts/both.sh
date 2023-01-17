c9fd8118bc5576c095d4c3b4a76ac850b254ec13
Mason Rhodes <mason@lifemd.com>
2023-01-17 10:26:52 -0800

    feat: add single script to build both linux installers

---
+++ scripts/both.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# set the working directory to the script directory
+cd $SCRIPT_DIR
+
+# Make .deb
+/bin/bash ./debian-build-pkg.sh
+
+# Package isntall script assets
+/bin/bash ./manual-install.sh
