55583fee673792a63725bf173d15f2cc2cac177e
Brian Miller <brian@phospher.com>
2023-01-22 19:04:55 -0800

    removing dynamic self-signed code signing keys to locally hosted sewcure storage--preparation for real public cert.

--- phvalheim-client-installer/signing/signIt.bat
+++ phvalheim-client-installer/signing/signIt.bat
@@ -2,23 +2,22 @@
 REM likely Debug or Release
 set buildType=%1
 set sdk=C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64
+set codeSignPfx=\\cerebrum.phospher.com\development\phvalheim-client\not_git\code_signing_certs\MyKey.pfx
 
 echo Build Type: %buildType%
 echo SDK: %sdk%
 
 REM create a self-signed cert
-"%sdk%\MakeCert.exe" /n CN=posixone -a SHA256 /r /h 0 /eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" /e 01/01/2055 /sv ..\signing\MyKey.pvk ..\signing\MyKey.cer
+REM "%sdk%\MakeCert.exe" /n CN=posixone -a SHA256 /r /h 0 /eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" /e 01/01/2055 /sv ..\signing\MyKey.pvk ..\signing\MyKey.cer
 
 REM convert the cert so we can sign code with it
-"%sdk%\Pvk2Pfx.exe" -f /pvk "..\signing\MyKey.pvk" /spc "..\signing\MyKey.cer" /pfx "..\signing\MyKey.pfx"
+REM "%sdk%\Pvk2Pfx.exe" -f /pvk "..\signing\MyKey.pvk" /spc "..\signing\MyKey.cer" /pfx "..\signing\MyKey.pfx"
+
+
+
 
 REM sign our msi
-REM "%sdk%\signtool.exe" sign /f "..\signing\MyKey.pfx" /tr http://timestamp.comodoca.com/rfc3161 /td SHA256 /fd SHA256 /v "..\%buildType%\phvalheim-client-installer.msi"
-
-
-
-
-
+"%sdk%\signtool.exe" sign /f %codeSignPfx% /tr http://timestamp.comodoca.com/rfc3161 /td SHA256 /fd SHA256 /v "..\%buildType%\phvalheim-client-installer.msi"
 
 
 
