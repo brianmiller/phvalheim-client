dc440f4585666e933777b35bba8311d46f9ff218
theoriginalbrian <gitbrian@phospher.com>
2023-01-30 11:34:34 -0800

    Update build_ubuntu_and_universal.sh

--- scripts/build_ubuntu_and_universal.sh
+++ scripts/build_ubuntu_and_universal.sh
@@ -26,9 +26,9 @@ bash both.sh
 rm -rf bin/ obj/
 
 # Let's see it
-ls -al /dooyet/phvalheim-client/published_build/*
+ls -al /dooyet/phvalheim-client/builds/*
 
 # Push to git
-git add /dooyet/phvalheim-client/published_build/*
+git add /dooyet/phvalheim-client/builds/*
 git commit -a -m "Automated Build: $PRETTY_NAME"
 git push --set-upstream origin master
