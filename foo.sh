cfe405c850da36149bb07a447c2726b7823c81e2
Brian Miller <brian@phospher.com>
2023-02-04 15:10:42 -0800

    build process

---
+++ foo.sh
@@ -0,0 +1,9 @@
+dotnet publish \
+                -c Release \
+        -r linux-x64 \
+        -p:PublishSingleFile=true \
+        --self-contained true \
+        ./phvalheim-client.csproj \
+        /property:GenerateFullPaths=true \
+        /consoleloggerparameters:NoSummary \
+                /p:PublishTrimmed=false
