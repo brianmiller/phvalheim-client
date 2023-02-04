e633ef21b5349fd018756ce41eefd61c59d77386
Brian Miller <brian@phospher.com>
2023-02-04 15:21:06 -0800

    build process

--- scripts/post_build_vss.bat
+++ scripts/post_build_vss.bat
@@ -4,16 +4,16 @@ set Configuration=%2
 
 
 
-mkdir %ProjectDir%\bin\%Configuration%\net6.0\linux-x64
+REM mkdir %ProjectDir%\bin\%Configuration%\net6.0\linux-x64
 mkdir %ProjectDir%\bin\%Configuration%\net6.0\win-x64
-copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\linux-x64\phvalheim-client.runtimeconfig.json
+REM copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\linux-x64\phvalheim-client.runtimeconfig.json
 copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\win-x64\phvalheim-client.runtimeconfig.json
 
 echo
 REM publish Windows x64 binaries
 dotnet publish -c %Configuration% -r win-x64 -p:PublishSingleFile=true --self-contained true .\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
 REM publish Linux x64 binaries
-dotnet publish -c %Configuration% -r linux-x64 -p:PublishSingleFile=true --self-contained true .\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
+REM dotnet publish -c %Configuration% -r linux-x64 -p:PublishSingleFile=true --self-contained true .\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
 echo
 
 echo Project Dir: %ProjectDir%
