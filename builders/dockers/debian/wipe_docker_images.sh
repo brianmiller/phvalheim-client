2a8b1dedf37a7d858ac45306ad13f477526ef1fa
Brian Miller <brian@phospher.com>
2023-02-05 13:03:14 -0800

    finally, a working dockerized linux build enviornment!

---
+++ builders/dockers/debian/wipe_docker_images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# image to delete
+image="debian-package-builder"
+
+
+# find docker
+docker=$(which docker 2> /dev/null)
+if [ $? = 0 ]; then
+        docker=$docker
+else
+        podman=$(which podman 2> /dev/null)
+        if [  $? = 0 ]; then
+                docker=$podman
+        else
+                echo "ERROR: Docker or Podman are required, exiting..."
+                exit 1
+        fi
+fi
+
+echo
+echo "Erasing all containers and the image from: \"$image\""
+$docker rm -f $image
