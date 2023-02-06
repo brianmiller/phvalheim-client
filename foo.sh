40319b87e7492a387b64d7a6d04f99bb0eb8270d
Brian Miller <brian@phospher.com>
2023-02-06 08:47:08 -0800

    revamping build process

--- foo.sh
+++
@@ -1,9 +0,0 @@
-dotnet publish \
-                -c Linux-Release \
-        -r linux-x64 \
-        -p:PublishSingleFile=true \
-        --self-contained true \
-        ./phvalheim-client.csproj \
-        /property:GenerateFullPaths=true \
-        /consoleloggerparameters:NoSummary \
-                /p:PublishTrimmed=false
