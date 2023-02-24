ebed53abdddc36d63fd7ca4f0d5014512477f62d
Brian Miller <brian@phospher.com>
2023-02-23 16:07:31 -0800

    gha

--- builders/post_build_vss.bat
+++ builders/post_build_vss.bat
@@ -10,10 +10,13 @@ REM copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.
 copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\win-x64\phvalheim-client.runtimeconfig.json
 
 echo
+
 REM publish Windows x64 binaries
 dotnet publish -c %Configuration% -r win-x64 -p:PublishSingleFile=true --self-contained true %ProjectDir%\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
+
 REM publish Linux x64 binaries
 REM dotnet publish -c %Configuration% -r linux-x64 -p:PublishSingleFile=true --self-contained true .\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
+
 echo
 
 echo Project Dir: %ProjectDir%
