5df1bba3f0c30ae725c106dd2bcee53d46206fc9
Brian Miller <brian@phospher.com>
2023-02-24 09:45:18 -0800

    fixed windows build errors

--- builders/post_build_vss.bat
+++ builders/post_build_vss.bat
@@ -3,21 +3,22 @@ set ProjectDir=%1
 set Configuration=%2
 
 
-
+REM Linux (not used, moved to dockerized builds for Linux)
 REM mkdir %ProjectDir%\bin\%Configuration%\net6.0\linux-x64
-mkdir %ProjectDir%\bin\%Configuration%\net6.0\win-x64
 REM copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\linux-x64\phvalheim-client.runtimeconfig.json
-copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\win-x64\phvalheim-client.runtimeconfig.json
-
-echo
-
-REM publish Windows x64 binaries
-dotnet publish -c %Configuration% -r win-x64 -p:PublishSingleFile=true --self-contained true %ProjectDir%\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
-
-REM publish Linux x64 binaries
 REM dotnet publish -c %Configuration% -r linux-x64 -p:PublishSingleFile=true --self-contained true .\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
 
-echo
 
+REM Windows
+mkdir %ProjectDir%\bin\%Configuration%\net6.0\win-x64
+mkdir %ProjectDir%\obj\%Configuration%\net6.0\win-x64
+copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.runtimeconfig.json %ProjectDir%\bin\%Configuration%\net6.0\win-x64\phvalheim-client.runtimeconfig.json
+copy %ProjectDir%\obj\%Configuration%\net6.0\apphost.exe %ProjectDir%\obj\%Configuration%\net6.0\win-x64\singlefilehost.exe
+copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.dll %ProjectDir%\obj\%Configuration%\net6.0\win-x64\phvalheim-client.dll
+copy %ProjectDir%\bin\%Configuration%\net6.0\phvalheim-client.pdb %ProjectDir%\obj\%Configuration%\net6.0\win-x64\phvalheim-client.pdb
+dotnet publish -c %Configuration% -r win-x64 -p:PublishSingleFile=true --self-contained true %ProjectDir%\phvalheim-client.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:PublishTrimmed=false --no-build
+
+
+echo
 echo Project Dir: %ProjectDir%
 echo Configuration: %Configuration%
